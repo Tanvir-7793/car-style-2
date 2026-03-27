25be69ee20a5c5c822ea7c1dbf0e0c5d7c65a710
Tanvir-7793 <tanvir@example.com>
2026-03-27 13:18:34 +0530

    adding vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ import TopInfoBar from "@/components/TopInfoBar";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -36,6 +36,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
